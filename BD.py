cdb2764f09c02cd27cbe1d58e581ee4b4149f32f
Jockel <62969046+JuanCanoPalacios@users.noreply.github.com>
2021-09-09 11:41:37 -0300

    Se hicieron try y excepts para evitar tracebacks al no conectar a la BD

--- BD.py
+++ BD.py
@@ -1,19 +1,31 @@
 import mysql.connector
 
-mydb = mysql.connector.connect(
-  host='127.0.0.1',
-  user='root',
-  password='',
-  database='Tienda_Electronica'
-)
-mycursor = mydb.cursor()
+try: 
+  mydb = mysql.connector.connect(
+    host='127.0.0.1',
+    user='root',
+    password='',
+    database='Tienda_Electronica'
+  )
+  mycursor = mydb.cursor()
+except mysql.connector.errors.DatabaseError:
+  print("No se pudo conectar a la base de datos...")
+  mydb = None
+  mycursor = None
 
-def EjecutarSQL(sql,val):
-  mycursor.executemany(sql, val)
-  mydb.commit()
+def EjecutarSQL_VAL(sql,val):
+  if (mycursor is None and mydb is None):
+    print ("No fue posible conectarse a la BD")
+  else:  
+    mycursor.executemany(sql, val)
+    mydb.commit()
 
 def Mostrar(Parametro):
-  mycursor.execute(Parametro)
-  myresult = mycursor.fetchall()
-  for x in myresult:
-    print(x)
+  if (mycursor is None and mydb is None):
+    print("No fue posible conectarse a la BD")
+  else:  
+    mycursor.execute(Parametro)
+    myresult = mycursor.fetchall()
+    for x in myresult:
+      print(x)
+
