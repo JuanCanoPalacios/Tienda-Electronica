40f0bbd84a995b9111cc84b539660c0a389dfed3
Jockel <62969046+JuanCanoPalacios@users.noreply.github.com>
2021-09-09 11:42:18 -0300

    Update Logica.py

--- Logica.py
+++ Logica.py
@@ -1,14 +1,18 @@
 import BD
+
 def MostrarTodosClientes():
     BD.Mostrar("SELECT * FROM Usuarios")
     
 def CrearNuevoUsuario(TipoUsuario):
     Nombre = input("Ingrese su nombre\n")
-    Telefono = input("Ingrese su apellido\n")
+    Telefono = input("Ingrese su telefono\n")
     Direccion = input("Ingrese email\n")
     sql = "INSERT INTO "+TipoUsuario+" (Nombre,Telefono,Direccion) VALUES (%s,%s,%s)"
     val  = [(Nombre,Telefono,Direccion)]
-    BD.EjecutarSQL(sql,val)
+    BD.EjecutarSQL_VAL(sql,val)
 
 def Mostrar(Tabla):
     BD.Mostrar("SELECT * FROM "+Tabla)
+
+def BorrarUsuario(ID):
+    BD.EjecutarSQL("DELETE FROM Clientes WHERE ID_Cliente="+ID)
