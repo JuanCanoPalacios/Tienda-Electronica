defdc036f754aaf9779e41633401ddd137569c18
MauroNahuelPaul <63022953+MauroNahuelPaul@users.noreply.github.com>
2021-11-07 22:45:26 -0300

    Update main.py

--- tienda/main.py
+++ tienda/main.py
@@ -32,7 +32,7 @@ def menuAdmin():
             confirmar()
         if(opcion=='5'):
             pass
-            #gestionarCompras()
+            gestionarCompras()
         if(opcion=='6'):
             gestionarVentas()
         elif(opcion=='9'):
@@ -56,24 +56,6 @@ def gestionarClientes():
         if(opcion=='9'):
             break
         
-
- def gestionarCompras():
-    while(1):
-        opcion = input("\n1. Mostrar las compras\n2. Nueva compra\n3. Borrar compra\n4. Modificar compra\n9. Salir\t")
-        if(opcion=='1'):
-            logica.mostrar("Compra")     
-            confirmar()
-        if(opcion=='2'):
-            logica.crearCompra()
-            confirmar()
-        if(opcion=='3'):
-            logica.borrarCompra(input("Ingrese el ID de la Compra que desea borrar: \t"))
-            confirmar()
-        if(opcion=='4'):
-            logica.modificarCompra(input("Ingrese el ID de la Compra a modificar: \t"))
-            confirmar()
-        if(opcion=='9'):
-            break        
 def gestionarProveedores():
     while(1):
         opcion = input("\n1. Mostrar todos los Proveedores\n2. Nuevo Proveedor\n3. Borrar Proveedor\n4. Modificar Proveedor\n9. Salir\t")
@@ -137,86 +119,30 @@ def gestionarVentas():
             logica.realizarVenta()
             confirmar()
         if(opcion=='3'):
-            logica.borrarVenta(input("Ingrese el ID de la venta a que desee dar de baja: \t"))
+            logica.borrarVenta(input("Ingrese el ID de la venta a que desee borar: \t"))
             confirmar()
         if(opcion=='4'):
             logica.modificarVenta(input("Ingrese el ID que desea modificar: \t"))
+            confirmar()
+
+def gestionarCompras():
+    while(1):
+        opcion = input("\n1. Mostrar historial de compras\n2. Realizar compra\n3. Eliminar compra\n4. Modificar compra\n9. Salir\t")
+        if(opcion=='1'):
+            logica.mostrar("Ventas")
+            confirmar()
+        if(opcion=='2'):
+            logica.crearCompra()
+            confirmar()
+        if(opcion=='3'):
+            logica.borrarCompra(input("Ingrese el ID de la compra a que desee borrar \t"))
+            confirmar()
+        if(opcion=='4'):
+            logica.modificarCompra(input("Ingrese el ID que desea modificar: \t"))
             confirmar()
 
 def confirmar():
     PEPE = input("\n\nPresione para 'Enter' continuar: \t")
     os.system('cls')
-       
-iniciarSesion()
-        opcion = input("\n1. Mostrar todos los Proveedores\n2. Nuevo Proveedor\n3. Borrar Proveedor\n4. Modificar Proveedor\n9. Salir\t")
-        if(opcion=='1'):
-            logica.mostrar("Proveedor")
-            confirmar()
-        if(opcion=='2'):
-            logica.crearProveedor()
-            confirmar()
-        if(opcion=='3'):
-            logica.borrarProveedor(input("Ingrese el ID del Proveedor que desea borrar: \t"))
-            confirmar()
-        if(opcion=='4'): 
-            logica.modificarProveedor(input("Ingrese el ID del Proveedor a modificar: \t"))
-            confirmar()
-        if(opcion=='9'):
-            break
-        
-def gestionarProductos():
-    while(1):
-        opcion = input("\n1. Mostrar todos los productos\n2. Alta productos\n3. Baja productos\n4. Modificar productos\n5. Alta producto proveedor\n6. Baja producto proveedor\n7. Modificar producto proveedor\n8. Mostrar producto proveedor\n9. Salir\t")
-        if(opcion=='1'):
-            logica.mostrar("Productos")
-            confirmar()
-        if(opcion=='2'):
-            logica.crearProductos()
-            confirmar()
-        if(opcion=='3'):
-            logica.borrarProductos(input("Ingrese el ID del Producto que desea borrar: \t"))
-            confirmar()
-        if(opcion=='4'):
-            logica.modificarProductos(input("Ingrese el ID del Producto a modificar: \t"))
-            confirmar()
-        if(opcion=='9'):
-            break
-def gestionarProductosProveedores():
-    while(1):
-        opcion = input("\n1. Mostrar producto proveedor\n2. Alta producto proveedor\n3. Baja producto proveedor\n4. Modificar producto proveedor\n9. Salir\t")
-        if(opcion=='1'):
-            logica.mostrar("Productos_Proveedor")
-            confirmar()
-        if(opcion=='2'):
-            logica.altaProductoProveedor()
-            confirmar()
-        if(opcion=='3'):
-            logica.bajaProductoProveedor(input("Ingrese el ID del Producto a que desee dar de baja: \t"))
-            confirmar()
-        if(opcion=='4'):
-            logica.modificarProductoProveedor(input("Ingrese el ID que desea modificar: \t"))
-            confirmar()
-        if(opcion=='9'):
-            break
-
-def gestionarVentas():
-    while(1):
-        opcion = input("\n1. Mostrar historial de ventas\n2. Realizar venta\n3. Eliminar venta\n4. Modificar venta\n9. Salir\t")
-        if(opcion=='1'):
-            logica.mostrar("Ventas")
-            confirmar()
-        if(opcion=='2'):
-            logica.realizarVenta()
-            confirmar()
-        if(opcion=='3'):
-            logica.borrarVenta(input("Ingrese el ID de la venta a que desee dar de baja: \t"))
-            confirmar()
-        if(opcion=='4'):
-            logica.modificarVenta(input("Ingrese el ID que desea modificar: \t"))
-            confirmar()
-
-def confirmar():
-    PEPE = input("\n\nPresione para 'Enter' continuar: \t")
-    os.system('clear')
        
 iniciarSesion()
