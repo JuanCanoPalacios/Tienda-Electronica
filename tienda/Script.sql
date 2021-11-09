715271c947daedd808cb58111a7d98b4d98dce5e
MauroNahuelPaul <63022953+MauroNahuelPaul@users.noreply.github.com>
2021-11-08 21:17:51 -0300

    Update Script.sql

--- tienda/Script.sql
+++ tienda/Script.sql
@@ -15,9 +15,9 @@ CREATE TABLE Clientes
 CREATE TABLE Productos
 (
     ID_Producto INT NOT NULL auto_increment,
+    Descripcion VARCHAR(100),
     Precio INT NOT NULL, 
     Cantidad INT NOT NULL,
-    Descripcion VARCHAR(100),
     PRIMARY KEY (ID_Producto)
 );
 CREATE TABLE Productos_Reservados
@@ -33,26 +33,25 @@ CREATE TABLE Proveedor
     ID_Proveedor INT NOT NULL auto_increment,
     Nombre VARCHAR(45) NOT NULL,
     Telefono INT NOT NULL,
-    PRIMARY KEY  (ID_Proveedor)
+    PRIMARY KEY (ID_Proveedor)
 );
 CREATE TABLE Productos_Proveedor
 (
+    ID_Productos_Proveedor INT NOT NULL auto_increment,
     ID_Producto INT NOT NULL,
-    ID_Productos_Proveedor INT NOT NULL,
     ID_Proveedor INT NOT NULL,
+    Precio INT NOT NULL,
     PRIMARY KEY (ID_Productos_Proveedor),
-    FOREIGN KEY (ID_Producto) REFERENCES Productos(ID_Producto),
+    FOREIGN KEY (ID_Producto) REFERENCES Productos (ID_Producto),
     FOREIGN KEY (ID_Proveedor) REFERENCES Proveedor (ID_Proveedor)
 );
 CREATE TABLE Compras
 (
     ID_Compra INT NOT NULL auto_increment,
-    ID_Proveedor INT NOT NULL,
     ID_Producto_Proveedor INT NOT NULL,
     Cantidad INT NOT NULL,
     Fecha DATETIME,
     PRIMARY KEY (ID_Compra),
-    FOREIGN KEY (ID_Proveedor) REFERENCES Proveedor(ID_Proveedor),
     FOREIGN KEY (ID_Producto_Proveedor) REFERENCES Productos_Proveedor(ID_Productos_Proveedor)
 );
 CREATE TABLE Ventas
